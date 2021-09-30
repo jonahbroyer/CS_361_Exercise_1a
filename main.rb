f1aeda0c94c222cb9a11164a98cf91a77d71145a
Jonah Broyer <jkbsk8er@yahoo.com>
2021-09-30 13:32:02 -0700

    Completed Assignment.

--- main.rb
+++ main.rb
@@ -8,3 +8,24 @@
 
 # Now print each of the Player objects in the array, such that we see something like "Ready Player N! Score: S"
 # for each player in the array.
+
+class Player
+
+    attr_reader :name, :score
+
+    def initialize(name, score)
+        @name = name
+        @score = score
+    end
+
+end
+
+players = []
+
+50.times do
+    players << Player.new("Player", "Score")
+end
+
+players.each_with_index do |player, i|
+    puts "Ready Player #{i + 1}! Score: #{rand(10..300)}"
+end
